b8e2c62b31a77403ada60ea20ee04a6f37bf7085
destroyer443 <whoever443@gmail.com>
2021-10-10 20:04:18 +0700

    Added CheckboxToggle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function ToDoTable(props) {
-  return (
-    <tr>
-      <td>
-        <label>{props.number}</label>
-      </td>
-      <td>
-        <label>{props.toDo}</label>
-      </td>
-      <td>
-        <label><input type="checkbox" /></label>
-      </td>
-      <td>
-        <label><button onClick = {props.remove}>Remove </button></label>
-      </td>
-    </tr>
-  )
+class ToDoTable extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>
+          <label>{this.props.number}</label>
+        </td>
+        <td>
+          <label>{this.props.toDo}</label>
+        </td>
+        <td>
+          <label>
+            <input
+              id={this.props.id}
+              type="checkbox"
+              defaultChecked={this.props.completed}
+              onChange={() => this.props.handleChecked(this.props.id)}
+            />
+          </label>
+        </td>
+        <td>
+          <label>
+            <button onClick={this.props.remove}>Remove </button>
+          </label>
+        </td>
+      </tr>
+    );
+  }
 }
 
 class InputBar extends React.Component {
@@ -32,9 +43,13 @@ class InputBar extends React.Component {
 
   render() {
     return (
-      <input type="text" placeholder="Type what you need to do..." 
-      value={this.props.toDo} onChange={this.handleChange} />
-    )
+      <input
+        type="text"
+        placeholder="Type what you need to do..."
+        value={this.props.toDo}
+        onChange={this.handleChange}
+      />
+    );
   }
 }
 
@@ -45,60 +60,101 @@ class ToDoList extends React.Component {
     this.state = {
       list: [],
       toDoCounter: toDoCounter,
+      toDo: "",
     };
-  
-  this.add = this.add.bind(this);
-  this.handleChange = this.handleChange.bind(this);
+
+    this.add = this.add.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChecked = this.handleChecked.bind(this);
   }
 
   handleChange(toDo) {
-    this.setState( {
-          toDo: toDo,
-        }
-    )
-    console.log(this.state.toDo);
+    this.setState(
+      {
+        toDo: toDo,
+      },
+      () => {
+        console.log(this.state.toDo);
+      }
+    );
+  }
+
+  handleChecked(id) {
+    this.state.list.map((todos) => {
+      if (id === todos.id) {
+        return { ...todos, completed: !todos.completed };
+      }
+      return todos;
+    });
+    this.setState(
+      {
+        list: this.state.list,
+      },
+      () => {
+        console.log(this.state.list);
+      }
+    );
   }
 
   add() {
+    this.setState(
+      {
+        list: [
+          ...this.state.list,
+          {
+            id: this.state.toDoCounter + 1,
+            toDo: this.state.toDo,
+            completed: false,
+          },
+        ],
+        toDoCounter: this.state.toDoCounter + 1,
+      },
+      () => {
+        console.log(this.state.list);
+      }
+    );
     this.setState({
-      list: [
-        ...this.state.list,
-        {
-          id: this.state.toDoCounter +1,
-          toDo: this.state.toDo,
-        }
-      ],
-      toDoCounter: this.state.toDoCounter +1,
-    })
-    this.setState({
-      toDo: '',
-    })
-    console.log(this.state.list);
+      toDo: "",
+    });
   }
 
   remove(index) {
-    this.state.list.splice(index,1);
-    this.setState({
-      list: this.state.list,
-    })
-    console.log(this.state.list);
+    this.state.list.splice(index, 1);
+    this.setState(
+      {
+        list: this.state.list,
+      },
+      () => {
+        console.log(this.state.list);
+      }
+    );
   }
 
-  render(){
+  render() {
     return (
       <div>
-      <h1>A Simple To-do List</h1>
+        <h1>A Simple To-do List</h1>
         <div>
           <InputBar toDo={this.state.toDo} onChange={this.handleChange} />
-          <button onClick= {this.add}>Add</button>
+          <button onClick={this.add}>Add</button>
         </div>
+        <br />
         <br />
         <div>
           <table>
-          {this.state.list.map((todos,index) => (
-          <ToDoTable key={todos.id} number={index+1} toDo={todos.toDo} 
-          remove= {() => this.remove(index)} />
-        ))}
+            <tbody>
+              {this.state.list.map((todos, index) => (
+                <ToDoTable
+                  key={todos.id}
+                  id={todos.id}
+                  number={index + 1}
+                  toDo={todos.toDo}
+                  remove={() => this.remove(index)}
+                  completed={todos.completed}
+                  handleChecked={this.handleChecked}
+                />
+              ))}
+            </tbody>
           </table>
         </div>
       </div>
